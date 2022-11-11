f0bbc79c83bfa7915b8ee24770572688740f7395
HectorRute98 <736259@unizar.es>
2022-11-11 18:40:44 +0100

    ElizaServerTest done

--- src/test/kotlin/websockets/ElizaServerTest.kt
+++ src/test/kotlin/websockets/ElizaServerTest.kt
@@ -3,7 +3,6 @@ package websockets
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*
@@ -37,17 +36,17 @@ class ElizaServerTest {
         assertEquals("The doctor is in.", list[0])
     }
 
-    @Disabled
+    // @Disabled
     @Test
     fun onChat() {
-        val latch = CountDownLatch(4)
+        val latch = CountDownLatch(5)
         val list = mutableListOf<String>()
 
         val client = ElizaOnOpenMessageHandlerToComplete(list, latch)
         container.connectToServer(client, URI("ws://localhost:$port/eliza"))
         latch.await()
-        // assertEquals(XXX, list.size) COMPLETE ME
-        // assertEquals(XXX, list[XXX]) COMPLETE ME
+        assertEquals(5, list.size)
+        assertEquals("Please don't apologize.", list[3])
     }
 }
 
@@ -67,8 +66,8 @@ class ElizaOnOpenMessageHandlerToComplete(private val list: MutableList<String>,
     fun onMessage(message: String, session: Session) {
         list.add(message)
         latch.countDown()
-        // if (COMPLETE ME) {
-        //    COMPLETE ME
-        // }
+        if (list.size == 3) {
+            session.basicRemote.sendText("sorry")
+        }
     }
 }
